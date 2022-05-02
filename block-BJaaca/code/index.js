4962aecb81f4efb7079e01edefaa8d6115547bfa
rcc0404 <rahulchaudhar3@gmail.com>
2022-05-02 07:56:44 +0530

    Object

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -1,12 +1,12 @@
 //  Follow instructions below and write code accordingly.
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
-// let character = 0; 
+let character = {}; 
 
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
-let character = {
+character = {
     characterName: "Arya"
 }
 
@@ -14,7 +14,7 @@ let character = {
 
 let age = 20;
 
-character.age = 24;
+character["character-age"] = age;
 
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
 
@@ -26,30 +26,57 @@ character.title = "Lady of Winterfell";
 
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
 
-character.greet = "I am Arya and my title is Lady of Winterfell }";
-
+character.greet = function () {
+    alert(
+        `I am ${character.characterName} and my title is ${character.title}`
+    );
+};
 // - Add a new property to the character object named `isFemale` and value will be `true`
 
-character.isFemale = "true";
+character.isFemale = true;
 
 // - Execute the function stored in the greet key of the character object
 
+character.greet();
+
 //  - Change the method `greet` to now alert `She is [NAME HERE] and her title is [TITLE HERE]`.
+
+character.greet = function () {
+    alert(
+        `She is ${character.characterName} and her title is ${character.title}`
+    );
+};
 
 // - Check by calling `greet` and see if the message changed or not.
 
+character.greet();
+
 //  - Add a new property in the character object `isAdult`. The value will be true if age is greater than 18 or it will be false.
 
+character.isAdult = character.age > 18 ? true : false;
+
 // - Add a new method (function inside objects are called methods) named `changeIsAdult`. When called should be able to flip the value of `isAdult` from true to false and false to true.
+
+character.changeIsAdult = function () {
+    //true => !true
+    //false => !false
+    character.isAdult = !character.isAdult;
+}
 
 // - Add a new key with the name stored in variable `keyName` defined below i.e `playedBy` (use [] for computed property) and the value of "Maisie Williams"
 
 let keyName = 'playedBy';
+character[keyName] = "Maisie Williams";
+
 
 // - Using `alert` alert the value stored in key you added above `keyName` use the variable name to access
+alert(character[keyName]);
 
 // - Using the function `console.log` log the value of the key `42`. You don't have to add the key
 
-// - Add a new key named `totalSeasons` with the value of `1 + 2 + 3 + 2`. Use this expression
+console.log(character[42]);
 
+// - Add a new key named `totalSeasons` with the value of `1 + 2 + 3 + 2`. Use this expression
+character.totalSeasons = 1 + 2 + 3 + 2;
 // - Access the value of the key `totalSeason` (it should be 8)
+console.log(character.totalSeasons);
