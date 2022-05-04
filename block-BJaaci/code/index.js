376d8bb3e45ef15691253642bf645fef88b42ade
rcc0404 <rahulchaudhar3@gmail.com>
2022-05-04 18:45:04 +0530

    array

--- block-BJaaci/code/index.js
+++ block-BJaaci/code/index.js
@@ -32,6 +32,8 @@ Use the variable named movies to solve the requirement below.
 
 // 1. Log the value of the title key in the first movie of the array named `movies`
 
+console.log[movies];
+
 // 2. Log the value of the title key in the last movie of the array named `movies`
 
 // 3. Log the value of the title key in the second movie (index 1) of the array named `movies`
